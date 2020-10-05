9e63cd794bafa141e74f4eedfd6c65d420d05a1d
Hoang Vu Truong <hoangvu.truong@rovisys.com>
2020-10-05 18:34:26 +0800

    Building Recipe List Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [ new Recipe('Yoghurt', 'How to make Yoghurt', 'https://media.healthyfood.com/wp-content/uploads/2017/04/HTC-natural-yoghurt-iStock_58226440.jpg')];
   constructor() { }
 
   ngOnInit(): void {
