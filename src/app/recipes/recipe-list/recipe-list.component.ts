7ab7f73d3333e7533d313c4f175374c05e7f0296
Vu Truong <hoangvutruong27894@gmail.com>
2020-10-06 22:26:10 +0800

    Completed Basic coursework Angular App

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,8 +7,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [ new Recipe('Yoghurt', 'How to make Yoghurt', 'https://media.healthyfood.com/wp-content/uploads/2017/04/HTC-natural-yoghurt-iStock_58226440.jpg')];
-  constructor() { }
+  recipes: Recipe[] = [ 
+    new Recipe('Yoghurt', 'How to make Yoghurt', 'https://media.healthyfood.com/wp-content/uploads/2017/04/HTC-natural-yoghurt-iStock_58226440.jpg'),
+    new Recipe('Tom Yam Soup', 'A delicious Thai-style soup', 'https://i0.wp.com/www.valerieskeepers.com/wp-content/uploads/2015/06/tom-yum-12-1-sur-1.jpg?fit=2048%2C1849')
+  ];
+  
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
   }
